b813cbcbe601dc03b30792aecbfa14bb1de33cb1
Thomas Binnie <35494388+binnietom@users.noreply.github.com>
2022-03-24 10:07:34 +0000

    likelihood updates
    chunks, toggles cov loading/estimation

--- py21cmmc_wv/likelihood.py
+++ py21cmmc_wv/likelihood.py
@@ -5,12 +5,13 @@ A module defining CosmoHammer likelihoods for addition into the standard 21cmMC 
 import numpy as np
 from powerbox.dft import fft
 from powerbox.tools import angular_average_nd
-from py21cmmc.mcmc import core, likelihood
+from py21cmmc import core, likelihood
 from scipy import special as sp
+import math as m
 
 from .morlet import morlet_transform_c
 from .util import lognormpdf
-
+from .util import loaded_lognormpdf
 
 class LikelihoodWaveletsMorlet(likelihood.LikelihoodBaseFile):
     """
@@ -21,74 +22,195 @@ class LikelihoodWaveletsMorlet(likelihood.LikelihoodBaseFile):
     """
     required_cores = [core.CoreLightConeModule]
 
-    def __init__(self, bins=None, model_uncertainty=0.15, **kwargs):
+    def __init__(self, bins=None, nchunks=1, model_uncertainty=1., stride=1, cov='est', **kwargs):
         super().__init__(**kwargs)
 
         self.bins = bins
         self.model_uncertainty = model_uncertainty
+        self.stride = stride
+        self.nchunks = nchunks
+        self.cov = cov
+        #Covariance will be estimated if 'est'; to load the covariance put cov='the path and file name'(-.npy).
 
     def computeLikelihood(self, model):
+        """
+        old likelihood function is inside the loop.
+        Calculates the likelihood in the same way as 21cmmc
+        we have w,k,k,c in a dictionary per chunk.
+        """
+        lnL=0
+        print("\n got to likelihood \n")
+        for i in range(self.nchunks): #loop through chunks# stack likelihood.
+            mu = model[i]['wavelets']
+            x = self.data[i]['wavelets']
 
-        model = model[0]
+            #mu = model['wavelets']  ##don't overwrite model!
+            if (self.cov == 'est'):
+                covariance = self.compute_covariance(mu, model[i]['kpar'], model[i]['centres'])
 
-        x = self.data[0]['wavelets']
-        mu = model['wavelets']
+                L = lognormpdf(
+                    x=x.reshape((-1, x.shape[-1])),
+                    mu=mu.reshape((-1, mu.shape[-1])),
+                    cov=covariance,
+                )
+            if (self.cov != 'est'):
+                print(f"loading cov {i}")
+                if (self.nchunks == 3):
+                    covariance = np.load(f"{self.cov}chunk_{i}.npy", allow_pickle=True)
+                if (self.nchunks == 1):
+                    covariance = np.load(f"{self.cov}.npy", allow_pickle=True)
+                print("loaded cov")
+                L = loaded_lognormpdf( #lognormpdf2(
+                    x=x.reshape((-1, x.shape[-1])),
+                    mu=mu.reshape((-1, mu.shape[-1])),
+                    cov=covariance,  #####. i for chunk. but we just load each time (better for memory this way)
+                )
 
-        # TODO: data/model should probably be transposed in some way.
-        return lognormpdf(
-            x=x.reshape((-1, x.shape[-1])),
-            mu=mu.reshape((-1, mu.shape[-1])),
-            cov=self.compute_covariance(model['wavelets'], model['kpar'], model['centres'])
-        )
+            if (m.isnan(L)):
+                print( 'Nan lnL ')
+                return np.inf
+            else:
+                lnL += L
+            print("\n likelihood for chunk ", i, " is ", L)
+        print("\n total Likelihood: ", lnL, "\n")
+        return lnL
 
     @staticmethod
-    def compute_mps(lightcone, bins=None, nthreads=None):
+    def compute_mps(lightcone, bins=None, nthreads=None, nchunks=1, stride=1, integral_width=5):
+        """
+        computing the mps on chunks of the lightcone - stacked in an array just like 21cmmc does for the FPS
+        This is structured so that the old compute_mps function is now compute_mt so that any compute_mps references don't break
+        If no chunks inputted, nchunks = 1, the whole lightcone is one chunk (equivalent to no chunking).
+        """
+        print("\n chunks: ", nchunks)
+        data = []
+        chunk_indices = list(range(0,lightcone.n_slices,round(lightcone.n_slices / nchunks),))
 
-        # First get "visibilities"
-        vis, kperp = fft(lightcone.brightness_temp, L=lightcone.user_params.BOX_LEN, axes=(0, 1))
+        if len(chunk_indices) > nchunks:
+            chunk_indices = chunk_indices[:-1]
+        chunk_indices.append(lightcone.n_slices)
 
-        # vis has shape (HII_DIM, HII_DIM, lightcone_dim)
+        print("\n entering chunk loop \n")
+        for i in range(nchunks):
+            start = chunk_indices[i]
 
-        # Do wavelet transform
-        wvlts, kpar, _ = morlet_transform_c(vis.T, lightcone.lightcone_coords, nthreads=nthreads)
+            try:
+                end = chunk_indices[i + 1]
+            except IndexError:
+                if i == nchunks-1 and nchunks >1:
+                    end = lightcont.n_slices
+                else: break
+            chunklen = (end - start) * lightcone.cell_size
 
-        # wvlts has shape (len(kpar) + vis.T.shape) corresponding to (eta, nu_c, u,v)
+            #wvlts, kperp_mod, kpar, centres = compute_mt(lightcone.lightcone_coords[start:end])
+            # ^this was buggy, just put old-code here Tom - wanted to avoid passing lightcones
+            print("\n chunk:", i," starting visibilities  \n")
+            print(f"\n visibility inputs: \n Tb: {lightcone.brightness_temp[:, :, start:end].shape} , \n Box:  {lightcone.user_params.BOX_LEN} \n \n")
 
-        # Now square it...
-        wvlts = np.abs(wvlts) ** 2
+            # First get "visibilities" with shape (HII_DIM, HII_DIM, lightcone_dim)
+            vis, kperp = fft(lightcone.brightness_temp[:, :, start:end], L=lightcone.user_params.BOX_LEN, axes=(0, 1))
 
-        # Determine a nice number of bins.
-        if bins is None:
-            bins = int((np.product(kperp.shape) * len(kpar)) ** (1. / 3.) / 2.2)
+            print("\n chunk:", i," visibilities done  \n")
+            centres = lightcone.lightcone_coords[start:end]
+            centres = centres[::stride] ## so if stride = 1 and nchunks = 1 centres = array of every pixel along the lightcone.
+            print("\n chunk:", i," entering MPS \n")
+            # Do wavelet transform
+            wvlts, kpar, _ = morlet_transform_c(vis.T, centres, convergence_extent=integral_width, nthreads=nthreads, stride = stride)
+            print("\n chunk:", i," MPS done  \n")
+            # Now remove complex.
+            wvlts = np.abs(wvlts) ** 2
+            # Determine a nice number of bins >70 or wavelet form changes.
+            if bins is None:
+                bins = int(49) ##why?
 
-        # And angularly average
-        wvlts, k = angular_average_nd(
-            wvlts.transpose((2, 3, 0, 1)),
-            list(kperp) + [kpar, lightcone.lightcone_coords],
-            n=3, bins=bins, bin_ave=False, get_variance=False
-        )
-
-        return wvlts, k, lightcone.lightcone_coords
+            print("\n chunk:", i," averaging  \n")
+            #before angularly average wavelets (transpose) has shape (kperp, kperp, kparr, centres)
+            wvlts, kperp_mod = angular_average_nd(
+                wvlts.transpose((2, 3, 0, 1)),
+                list(kperp) + [kpar, centres],
+                n=2, bins=bins, bin_ave=False, get_variance=False
+            )
+            #n=2 takes angular average of first two inputs, which when wavelet is transposed is k_par,k_par.
+            #wvlts now has shape (kperpmod, k_par, centres)
+            print("\n chunk:", i," appending dictionary with averaged MPS  \n")
+            data.append({"wavelets": wvlts, "kperp_mod": kperp_mod, "kpar": kpar, "centres": centres})
+        return np.array(data)
 
     def compute_covariance(self, wvlts, kpar, dist):
-
-        # wvlts (from compute_mps) has shape (Nk, Nz).
-        # only the Nz will correlate (on the assumption that power is independent in each mode).
-
+        """
+        wvlts (from compute_mps) has shape (N_kperp, Nk_parr, N_xz).
+        only the z direction kparr will correlate (on the assumption that power is independent in each k_perp mode).
+        """
         cov = []
         D = np.abs(np.add.outer(dist, -dist))
 
         for ix in range(wvlts.shape[0]):
             for ikp in range(wvlts.shape[1]):
                 wvlt = wvlts[ix, ikp]
-                thiscorr = (1 - sp.erf(kpar[ikp] * D / (2 * np.sqrt(2)))) ** 2
-                cov.append(thiscorr * self.model_uncertainty ** 2 * np.outer(wvlt, wvlt))
+                thiscorr = np.exp((-1/4)*kpar[ikp]*kpar[ikp]*D)
+                cov.append(thiscorr * (self.model_uncertainty ** 2) * np.outer(wvlt, wvlt))
+        return np.array(cov)
 
-        return cov
+    @staticmethod
+    def comp_cov_check(mw_realisation):
+        """
+        Calculating the Covariance directly from multiple wavelets
+        expects [N, [wavelet(kperp, kparr, centres), kperpM,  kparr, centres]] from N different lightcone realisations
+        returns kperp x kparr covariance matrices (centres, centres) measured from N realisations
+        """
+        covs = []
+        centres = mw_realisation[0,3] #lc coords all the same, just take 1st.
+        lenc = len(centres)
+        wvlts = []
+        for ireal in range(len(mw_realisation)):
+            wvlts.append(mw_realisation[ireal,0]) #get array of just the  wavelets
+        wvlts = np.array(wvlts)
+
+        for ikperp in range(len(wvlts[0,:,0,0])):
+            cov = []
+            for ikparr in range(len(wvlts[0,0,:,0])):
+                wvlt = wvlts[:,ikperp, ikparr, :] #[realisations, centres] per [kperp, kparr]
+                c = np.zeros((lenc,lenc))
+                for xi_c in range(lenc):
+                    for xj_c in range(lenc):
+                        c[xi_c,xj_c] = np.mean(np.dot(wvlt[:,xi_c],wvlt[:,xj_c])) - np.mean(wvlt[:,xi_c])*np.mean(wvlt[:,xj_c])
+                cov.append(c)
+            covs.append(cov)
+        return np.array(covs)/len(mw_realisation) # Divide by N realisations!
+
+    @staticmethod
+    def cov_FPS_comparison(fps_realisation):
+        """
+        Calcualting the covariance between N realisations of the FPS.
+        Similar to comp_cov_check but for comparisons with to FPS
+        expects [N, [nchunks, {"k":k, "delta":delta}]]
+        returns nchunks covariance matrices (k, k) measured from N realisations.
+        """
+        covs = []
+        nchunks = len(fps_realisation[0]) #same for each N
+        #start cov loops
+        for ichunk in range(nchunks):
+            ks = fps_realisation[0][ichunk]["k"] #same for each N
+            lenk = len(ks)
+            FPSs = [] #get array of just FPS
+            for i in range(len(fps_realisation)):
+                FPSs.append(fps_realisation[i][ichunk]["delta"])
+            FPSs = np.array(FPSs)
+            cov = np.zeros((lenk,lenk))
+            for ik in range(lenk):
+                for jk in range(lenk):
+                    cov[ik,jk] = np.mean(np.dot(FPSs[:,ik],FPSs[:,jk])) - np.mean(FPSs[:,ik])*np.mean(FPSs[:,jk])
+                    if (m.isnan(cov[ik,jk])): #when an array has nan's they are all nans - set to zero for plotting.
+                        cov[ik,jk] = 0
+                        print(" FPSs array has nans (chunk, k)  (", ichunk," ,  " , ik , ") ")
+            covs.append(cov)
+        return np.array(covs)/len(fps_realisation)
+
 
     def reduce_data(self, ctx):
-        wvlt, k, centres = self.compute_mps(ctx.get("lightcone"), self.bins)
-        return [dict(wavelets=wvlt, k=k, centres=centres)]
+        #wvlt, kperp_mod, kpar,  centres = self.compute_mps(ctx.get("lightcone"), self.bins, stride = self.stride)
+        #return [dict(wavelets=wvlt, kperp_mod=kperp_mod, kpar=kpar, centres=centres)]
+        return self.compute_mps(ctx.get("lightcone"), self.bins, nchunks = self.nchunks, stride = self.stride)
 
     @property
     def lightcone_module(self):
--- py21cmmc_wv/util.py
+++ py21cmmc_wv/util.py
@@ -1,6 +1,55 @@
 from numpy.linalg import slogdet, solve, LinAlgError
 import numpy as np
+import math
 import warnings
+
+from scipy.stats import multivariate_normal
+from scipy.sparse.linalg import spsolve
+
+def lognormpdf(x,mu,cov):
+    """
+    Calculate gaussian probability density of x, when x ~ N(mu,sigma)
+    """
+    L = 0
+    for i in range(len(x)):
+        nx = len(cov[i])
+        norm_coeff = nx*math.log(2*math.pi)+slogdet(cov[i])[1]
+        err = x[i]-mu[i]
+        numerator = spsolve(cov[i], err).T.dot(err)
+        L += -0.5*(norm_coeff+numerator)
+    return L
+
+def loaded_lognormpdf(x,mu,cov):
+    """
+    As lognormpdf but adapted to cater for loading cov matrix. (6192 = 49*129)
+    Currently 1 chunk cov with 260x260 centres
+    Within the nchunks loop:
+    Loaded npy shape is (nchunks, kperpmod, kpar, centres, centres)
+    Estimated shape is (kperpmod x kpar, centre, centres).
+    """
+    L = 0
+    nx = len(x)
+    for i in range(int(nx-len(cov[0,:,0,0]))): #(len(kperp_mod)-1)xlen(kparr)
+        iwrap_kparr = int(i - np.floor(i/len(cov[0,:,0,0]))*len(cov[0,:,0,0]))
+        iwrap_kperp = int(np.floor(i/len(cov[0,:,0,0])))
+        cv = cov[iwrap_kperp,iwrap_kparr,:,:]
+        err = x[i]-mu[i]
+        #try:
+        norm_coeff = nx*math.log(2*math.pi)+slogdet(cv)[1]
+        #except:
+        numerator = spsolve(cv, err).T.dot(err)
+        L += -0.5*(norm_coeff+numerator)
+    return L
+
+def straightlognorm(x,mu,cov):
+    """
+    a least code check for when block diagonal causes issues (typically from zeros)
+    """
+    L = 0
+    for i in range(len(x)):
+        for j in range(len(x[i])):
+            L += np.log(multivariate_normal(x[i], cov[i]).pdf(mu[i]))
+    return L
 
 
 def logdet_block_matrix(S):
@@ -15,7 +64,6 @@ def logdet_block_matrix(S):
                 warnings.warn("log-determinant not working: %s"%s)
 
     return sm
-
 
 def solve_block_matrix(S, x):
     bits = []
@@ -34,24 +82,25 @@ def solve_block_matrix(S, x):
                 warnings.warn("solve didn't work for index %s" % i)
 
     bits = np.array(bits)
-    return bits, inds
+    return bits #, inds
 
 
-def lognormpdf(x, mu, cov):
+def solve_block_matrix_T(S, x):
+    matrix = solve_block_matrix(S, x)
+    return solve_block_matrix[0].T
+
+def lognormpdf_sparse(x, mu, cov):
     """
     Calculate gaussian probability log-density of x, when x ~ N(mu,cov), and cov is block diagonal.
 
     Code adapted from https://stackoverflow.com/a/16654259
     """
+
     nx = len(x)
     norm_coeff = nx * np.log(2 * np.pi) + logdet_block_matrix(cov)
-
     err = x - mu
     sol, inds = solve_block_matrix(cov, err)
-
-    # The "inds" here means we only use the u co-ordinates that had a solution to the covariance.
     numerator = 0
     for i, (s, e) in enumerate(zip(sol, err[inds])):
         numerator += s.dot(e)
-
     return -0.5 * (norm_coeff + numerator)
